9c8544f0bb3224b5a1481b1dc9d0ae804264c59e
Leonardo Monteiro dos Santos <leeumonteiro@gmail.com>
2024-03-05 15:20:50 -0300

    first commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Qual operação deseja fazer:");
+           Console.WriteLine("1 - Adição");
+           Console.WriteLine("2- Subtração");
+           Console.WriteLine("3- Multiplicação");
+           Console.WriteLine("3- Divisão \n");
+
+            int operacao = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro número:  ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segund numero:  ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = 0;
+
+            switch (operacao)
+            {
+                case 1:
+                    {
+                        resultado = Adicao(num1, num2);
+                        break;
+                    }
+
+                case 2:
+                    {
+                        resultado = Subtracao(num1, num2);
+                        break;
+                    }
+
+                case 3:
+                    {
+                        resultado = Divisao(num1, num2); 
+                        break;
+                    }
+
+                case 4:
+                    {
+                        resultado = Multiplicacao(num1, num2);
+                        break;
+                    }
+
+                default:
+                    Console.WriteLine("Número invalido digite outro número");
+                    break;
+            }
+
+            Console.WriteLine("O resultado da operação com os números {0} e {1} é: {2}", num1, num2, resultado);
+
+            Console.ReadLine();
+        }
+
+
+        // creiando um metodo para cada operação
+        public static int Adicao(int numero1, int numero2)
+        {
+            int result = numero1 + numero2;
+            return result;
+        }
+
+        public static int Subtracao(int numero1, int numero2)
+        {
+            int result = numero1 - numero2;
+            return result;
+        }
+        public static int Divisao(int numero1, int numero2)
+        {
+            int result = numero1 * numero2;
+            return result;
+        }
+
+        public static int Multiplicacao(int numero1, int numero2)
+        {
+            int result = numero1 / numero2;
+            return result;
+        }
+    }
+}
---
+++ JogoPerguntas/JogoPerguntas/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace JogoPerguntas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pontuacao = 0;
+
+            Console.WriteLine("Quanto é 25 + 25? ");
+            int pergunta1= Convert.ToInt32(Console.ReadLine());
+           
+            // Pergunta 1
+            if (pergunta1 == 50)
+            {
+                Console.WriteLine("Você acertou");
+                pontuacao++;
+            }
+            else
+            {
+                Console.WriteLine("Você errou");
+            }
+
+            Console.WriteLine("\n**********************\n");
+
+            // Pergunta 2
+            Console.WriteLine("Quanto é 32 * 2? ");
+            int pergunta2= Convert.ToInt32(Console.ReadLine());
+           
+            if (pergunta2== 64)
+            {
+                Console.WriteLine("Você acertou");
+                pontuacao++;
+            }
+            else
+            {
+                Console.WriteLine("Você errou");
+            }
+
+            Console.WriteLine("\n**********************\n");
+
+            // Pergunta 3
+            Console.WriteLine("Quem descobriu o Brasil? ");
+            string pergunta3 = Console.ReadLine();
+           
+            if (pergunta3 == "Pedro Alvares Cabral")
+            {
+                Console.WriteLine("Você acertou");
+                pontuacao++;
+            }
+            else
+            {
+                Console.WriteLine("Você errou");
+            }
+
+            Console.WriteLine("\n**********************\n");
+
+            // Pergunta 4
+            Console.WriteLine("Quanto é 13.5 + 3? ");
+            float pergunta4 = float.Parse(Console.ReadLine());
+
+            if (pergunta4 == 16.5)
+            {
+                Console.WriteLine("Você acertou");
+                pontuacao++;
+            }
+            else
+            {
+                Console.WriteLine("Você errou");
+                
+            }
+
+            // Pontuação
+            Console.WriteLine("Sua pontuação é:"+pontuacao);
+
+
+        }
+    }
+
+}
